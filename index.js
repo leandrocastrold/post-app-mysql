858a0176f9010a097511c766b16a0c32b0a1f1e9
Leandro Castro <leandrocastrold@hotmail.com>
2020-10-05 21:41:30 -0300

    Handlebars initial settings added

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const handlebars = require('express-handlebars');
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars');
+//Database Connection
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postapp', 'root', '12345', {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+//Routes
 app.get('/', (req, res) => {
     res.send("Home Page");
 })
